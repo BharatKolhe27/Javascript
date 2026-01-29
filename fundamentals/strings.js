0524dd6fe538bfbf9eb8c3f66bc7ab4e1a9bc4bd
BharatKolhe27 <bharat.kolhe@gammaedge.io>
2026-01-29 18:17:05 +0530

    first commit

---
+++ fundamentals/strings.js
@@ -0,0 +1,7 @@
+let city = "Indore";
+console.log(city[0]); // "I"
+
+console.log(city.length);      // 6
+console.log(city.toUpperCase()); // "INDORE"
+console.log(city.trim());        // IT removes extra spaces
+console.log(city.toLowerCase()); 
---
+++ fundamentals/variables.js
@@ -0,0 +1,6 @@
+var name = "Bharat";
+let age = 22;
+const birthYear = 2002;
+
+// age = 23;      // Allowed
+// birthYear = 2005; // this will be Error since we cant update the value in const
